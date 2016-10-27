a1bfcc00feb2b0f35468b432d12255a0c497b802
Alexbgz <alexbgz@yandex.ru>
2016-10-27 16:12:36 +0300

    Add files via upload

--- ex_1.py
+++ ex_1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from librip.gens import field
+from librip.gen import field
+from librip.gen import gen_random
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
@@ -8,4 +9,7 @@ goods = [
     {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
 ]
 
+print([x for x in field(goods, 'price')])
+print([x for x in field(goods, 'title', 'price')])
+print([x for x in gen_random(1, 10, 7)])
 # Реализация задания 1
--- ex_6.py
+++ ex_6.py
@@ -3,15 +3,17 @@ import json
 import sys
 from librip.ctxmngrs import timer
 from librip.decorators import print_result
-from librip.gens import field, gen_random
+from librip.gen import field, gen_random
 from librip.iterators import Unique as unique
-
-path = None
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
 
-with open(path) as f:
+#assert len(sys.argv) > 0
+#path = sys.argv[0]
+path = "data_light.json"
+
+with open(path, encoding="utf8") as f:
     data = json.load(f)
 
 
@@ -23,23 +25,23 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return list(unique(list(field(arg, "job-name")), ignore_case=True))
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda _: "Программист" in _, arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda x: x + " с опытом Python", arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
-
+    return list(map(lambda x: "{}, зарплата {} руб.".format(x[0], x[1]),
+                    zip(arg, gen_random(100000, 200000, len(arg)))))
 
 with timer():
     f4(f3(f2(f1(data))))
